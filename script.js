06dcac09b224b2ebd791f990488146528db5e763
stein-rip <stein@stein.rip>
2023-01-31 10:20:29 -0500

    arrays update

--- script.js
+++ script.js
@@ -1,118 +1,131 @@
 // CODE FOR TIMER
-
-var countDownDuration = 300;
-var x = setInterval(function () {
-  countDownDuration--;
-  var minutes = Math.floor(countDownDuration / 60);
-  var seconds = countDownDuration % 60;
-  document.getElementById("timer").innerHTML = minutes + " m" + seconds + "s";
-  if (countDownDuration < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "TIMES UP!";
-  }
+let header = document.querySelector(header);
+setInterval(() => {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  header.textContent = `${hours}:${minutes}:${seconds}`;
 }, 1000);
 
 // CODE FOR GAME
 
-const cards = document.querySelectorAll(".card");
-const startButton = document.querySelector("#start-button");
-const resetButton = document.querySelector("#reset-button");
-const timer = document.querySelector("#timer");
-let flippedCards = [];
-let matchedCards = [];
-let startTime;
+const softBtn = document.querySelector(".soft-button");
+const hardBtn = document.querySelector(".hard-button");
+const difficultyBtns = document.querySelector(".difficulty-buttons");
+const main = document.querySelector("main");
+const hardContainer = document.querySelector(".hard-container");
+const softContainer = document.querySelector(".soft-container");
+const softCards = [
+  {
+    type: "tama-1",
+    url: "assets/tama-card-1.png",
+  },
+  {
+    type: "tama-1",
+    url: "assets/tama-card-1-2.png",
+  },
+  {
+    type: "tama-2",
+    url: "assets/tama-card-2.png",
+  },
+  {
+    type: "tama-2",
+    url: "assets/tama-card-2-2.png",
+  },
+  {
+    type: "tama-3",
+    url: "assets/tama-card-3.png",
+  },
+  {
+    type: "tama-3",
+    url: "assets/tama-card-3-2.png",
+  },
 
-startButton.addEventListener("click", startGame);
-resetButton.addEventListener("click", resetGame);
-
-cards.forEach((card) => {
-  card.addEventListener("click", flipCard);
-});
-
-function startGame() {
-  // shuffle cards and set up game
-  startTime = new Date();
-}
-
-function resetGame() {
-  // reset game board and variables
-}
-
-function flipCard() {
-  // flip card and check for matches
-  if (flippedCards.length === 2) {
-    // check if cards match and remove them if they do
-  } else {
-    // flip card back over if it doesn't match
-  }
-}
-
-// const cards = document.querySelectorAll(".card");
-// let hasFlippedCard = false;
-// let lockBoard = false;
-// let firstCard, secondCard;
-
-// function flipCard() {
-//   if (lockBoard) return;
-//   if (this === firstCard) return;
-
-//   this.classList.add("flipped");
-
-//   if (!hasFlippedCard) {
-//     hasFlippedCard = true;
-//     firstCard = this;
-
-//     return;
-//   }
-
-//   secondCard = this;
-//   checkForMatch();
-// }
-
-// function checkForMatch() {
-//   let isMatch;
-// }
-// let cards = [
-//   ["Ace", "Spades"],
-//   ["Two", "Spades"],
-//   ["Three", "Spades"],
-//   ["Ace", "Hearts"],
-//   ["Two", "Hearts"],
-//   ["Three", "Hearts"],
-//   ["Ace", "Clubs"],
-//   ["Two", "Clubs"],
-//   ["Three", "Clubs"],
-//   ["Ace", "Diamonds"],
-//   ["Two", "Diamonds"],
-//   ["Three", "Diamonds"],
-// ];
-
-// let cardGrid = [];
-// let gridSize = 4;
-
-// for (let i = 0; i < gridSize; i++) {
-//   cardGrid[i] = [];
-//   for (let j = 0; j < gridSize; j++) {
-//     let randomCardIndex = Math.floor(Math.random() * cards.length);
-//     let randomCard = cards.splice(randomCardIndex, 1)[0];
-//     cardGrid[i][j] = randomCard;
-//   }
-// }
-
-// console.log(cardGrid);
-
-// for (let i = 0; i < gridSize; i++) {
-//   for (let j = 0; j < gridSize; j++) {
-//     let currentCard = cardGrid[i][j];
-//     if (cardGrid[i][j + 1] && currentCard[0] === cardGrid[i][j + 1][0]) {
-//       console.log(`Cards match: ${currentCard} and ${cardGrid[i][j + 1]}`);
-//     }
-//     if (
-//       cardGrid[i + 1] &&
-//       cardGrid[i + 1][j] &&
-//       currentCard[0] === cardGrid[i + 1][j][0]
-//     ) {
-//       console.log(`Cards match: ${currentCard} and ${cardGrid[i + 1][j]}`);
-//     }
-//   }
-// }
+  {
+    type: "tama-4",
+    url: "assets/tama-card-4.png",
+  },
+  {
+    type: "tama-4",
+    url: "assets/tama-card-4-2.png",
+  },
+  // {
+  //   type: "tama-off",
+  //   url: "assets/tama-card-off",
+  // },
+];
+const hardCards = [
+  {
+    type: "sigil-1",
+    url: "assets/hard-card-1.jpg",
+  },
+  {
+    type: "sigil-1",
+    url: "assets/hard-card-1-2.jpg",
+  },
+  {
+    type: "sigil-2",
+    url: "assets/hard-card-2.jpg",
+  },
+  {
+    type: "sigil-2",
+    url: "assets/hard-card-2-2.jpg",
+  },
+  {
+    type: "sigil-3",
+    url: "assets/hard-card-3.jpg",
+  },
+  {
+    type: "sigil-3",
+    url: "assets/hard-card-3-2.jpg",
+  },
+  {
+    type: "sigil-4",
+    url: "assets/hard-card-4.jpg",
+  },
+  {
+    type: "sigil-4",
+    url: "assets/hard-card-4-2.jpg",
+  },
+  {
+    type: "sigil-5",
+    url: "assets/hard-card-5.jpg",
+  },
+  {
+    type: "sigil-5",
+    url: "assets/hard-card-5-2.jpg",
+  },
+  {
+    type: "sigil-6",
+    url: "assets/hard-card-6.jpg",
+  },
+  {
+    type: "sigil-6",
+    url: "assets/hard-card-6-2.jpg",
+  },
+  {
+    type: "sigil-7",
+    url: "assets/hard-card-7.jpg",
+  },
+  {
+    type: "sigil-7",
+    url: "assets/hard-card-7-2.jpg",
+  },
+  {
+    type: "sigil-8",
+    url: "assets/hard-card-8.jpg",
+  },
+  {
+    type: "sigil-8",
+    url: "assets/hard-card-8-2.jpg",
+  },
+  {
+    type: "sigil-8",
+    url: "assets/hard-card-8-2.jpg",
+  },
+  {
+    type: "hard-back",
+    url: "assets/hard-card-back.jpg",
+  },
+];
